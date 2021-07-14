441372367e197bc7fe1bcff9197f67fac15caa39
Vinicius Pereira de Oliveira <deoliveiradev@gmail.com>
2021-07-14 17:09:54 -0300

    Atualizando Charizard

--- treino-app/src/App.vue
+++ treino-app/src/App.vue
@@ -15,7 +15,5 @@ export default {
   components: {
     
   },
-
-
 };
 </script>
--- treino-app/src/views/ChariCard.vue
+++ treino-app/src/views/ChariCard.vue
@@ -1,12 +1,31 @@
 <template>
-  <v-container class="chari-card">
-    
-  </v-container>
+    <v-parallax height="600" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-img :src="PokeInfo.sprites.other['official-artwork'].front_default"></v-img>
+      
+      
+      <ul>
+        <li>Name: {{PokeInfo.name}}</li>
+        <li>Weight: {{PokeInfo.weight}}Kg</li>
+        <li>Height: {{PokeInfo.height}}M</li>
+      </ul>
+    </v-parallax>
 </template>
 
 <script>
   export default {
     name: 'ChariCard',
-
+    data() {
+      return {
+        PokeInfo: []
+      }
+    },
+    created() {
+      fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+      .then(resposta => resposta.json())
+      .then(json => {
+        this.PokeInfo = json
+        console.log(this.PokeInfo)
+      })
+    }
   }
 </script>
